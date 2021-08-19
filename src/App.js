ac1b80d4f773be7231a51fec90f75d90d26f07bf
juan camacho <juanc@tiv.solutions>
2021-08-18 22:30:34 -0400

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import Products from './components/products/Products'
+import React from "react";
+// import Products from './components/products/Products'
+// import Navbar from './components/Navbar/Navbar'
+
+import { Products, Navbar } from "./components";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
