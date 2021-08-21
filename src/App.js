6e905473ef00ebe1c36f1ded5cd576e6a5812e4e
juan camacho <juanc@tiv.solutions>
2021-08-21 14:54:53 -0400

    react router for cart and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
 import { Products, Navbar, Cart } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState({})
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -13,28 +14,36 @@ function App() {
   };
 
   const fetchCart = async () => {
-    setCart(await commerce.cart.retrieve())
-  }
+    setCart(await commerce.cart.retrieve());
+  };
 
   const handleAddToCart = async (productId, quantity) => {
     const item = await commerce.cart.add(productId, quantity);
 
     setCart(item.cart);
-  }
+  };
 
   useEffect(() => {
     fetchProducts();
     fetchCart();
   }, []);
 
-  console.log(cart.line_items)
+  console.log(cart.line_items);
 
   return (
-    <div>
-      <Navbar totalItems={cart.total_items}/>
-      {/* <Products products={products} onAddToCart={handleAddToCart}/> */}
-      <Cart cart={cart}/>
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
