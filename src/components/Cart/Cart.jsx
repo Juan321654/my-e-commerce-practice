7b436c356f8541d882e48fbf99ec3a265df63810
juan camacho <juanc@tiv.solutions>
2021-08-21 15:19:23 -0400

    adding and sub items works, emptying cart too

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
-import {Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 import useStyles from "./styles";
 import CartItem from "./CartItem/CartItem";
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
   const classes = useStyles();
 
   const EmptyCart = () => (
-    <Typography variant="subtitle1">You have no items in your cart.
-      <Link to="/" className={classes.link}>start adding some!</Link>
+    <Typography variant="subtitle1">
+      You have no items in your cart,&nbsp;
+      <Link to="/" className={classes.link}>
+        start adding some!
+      </Link>
     </Typography>
   );
 
@@ -19,7 +27,11 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item} />
+            <CartItem
+              item={item}
+              onUpdateCardQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCart}
+            />
           </Grid>
         ))}
       </Grid>
@@ -34,6 +46,7 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick={handleEmptyCart}
           >
             Empty cart
           </Button>
