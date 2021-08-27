5973a218b1e6ddac3c5c85cfbcf4933eeb6b3368
juan camacho <juanc@tiv.solutions>
2021-08-27 17:52:39 -0400

    fixed removing items in cart

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,36 +1,29 @@
-import React from "react";
-import {
-  Typography,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-} from "@material-ui/core";
+import React from 'react';
+import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 
-import useStyles from "./styles";
+import useStyles from './styles';
 
-const CartItem = ({ item, onUpdateCardQty, onRemoveFromCart }) => {
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
   const classes = useStyles();
 
+  const handleUpdateCartQty = (lineItemId, newQuantity) => onUpdateCartQty(lineItemId, newQuantity);
+
+  const handleRemoveFromCart = (lineItemId) => onRemoveFromCart(lineItemId);
+
   return (
-    <Card>
-      <CardMedia
-        image={item.media.source}
-        alt={item.name}
-        className={classes.media}
-      />
+    <Card className="cart-item">
+      <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
       <CardContent className={classes.cardContent}>
         <Typography variant="h4">{item.name}</Typography>
         <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
       </CardContent>
-      <CardActions className={classes.cartActions}>
+      <CardActions className={classes.cardActions}>
         <div className={classes.buttons}>
-          <Button type="button" size="small" onClick={() => onUpdateCardQty(item.id, item.quantity - 1)}>-</Button>
-          <Typography>{item.quantity}</Typography>
-          <Button type="button" size="small" onClick={() => onUpdateCardQty(item.id, item.quantity + 1)}>+</Button>
+          <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
+          <Typography>&nbsp;{item.quantity}&nbsp;</Typography>
+          <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
         </div>
-        <Button variant="contained" type="button" color="secondary" onClick={() => onRemoveFromCart(item.id)}>Remove</Button>
+        <Button variant="contained" type="button" color="secondary" onClick={() => handleRemoveFromCart(item.id)}>Remove</Button>
       </CardActions>
     </Card>
   );
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -8,6 +8,7 @@ import {
   CircularProgress,
   Divider,
   Button,
+  CssBaseline
 } from "@material-ui/core";
 
 import { commerce } from "../../../lib/commerce";
@@ -62,6 +63,7 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     );
   return (
     <>
+    <CssBaseline />
       <div className={classes.toolbar} />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
